452cdf301e25a4309eef3029b03167c6a4d0e1a9
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-22 13:24:48 -0300

    Update ReprodutorMusical.java
    Adicionando funções.

--- src/Funcionalidades.java/ReprodutorMusical.java
+++ src/Funcionalidades.java/ReprodutorMusical.java
@@ -1,5 +1,22 @@
 package reprodutor;
 
 public class ReprodutorMusical extends Funcionalidades{
-  
+  public class SelecionandoMusica {
+    public static void main (String[] args) {
+      System.out.printIn("Selecione a música desejada")
+    }
+  }
+  public class ReprodutorMusical {
+  boolean tocando=true;
+  String musica="1";
+
+    public void tocar(){
+      tocando=true;
+      System.out.printIn("Sua música está tocando")
+    }
+    public void pausar(){
+      tocando=false;
+      System.out.printIn("Sua música está pausada.");
+    }
+  }
 }
