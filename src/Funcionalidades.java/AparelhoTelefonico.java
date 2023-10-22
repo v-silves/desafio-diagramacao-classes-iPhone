ec9984a3c8a929a0398cd1622fe5446b2fbbe102
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-22 14:09:39 -0300

    Update AparelhoTelefonico.java
    Adicionando funções.

--- src/Funcionalidades.java/AparelhoTelefonico.java
+++ src/Funcionalidades.java/AparelhoTelefonico.java
@@ -1,5 +1,20 @@
 package aparelho;
 
 public class AparelhoTelefonico extends Funcionalidades {
-  
+  public static void main (String[] args) {
+    boolean tocando=true;
+    boolean atender=true;
+  }
+    public void aparelhoTocando() {
+      tocando=true;
+      System.out.printIn("Seu aparelho está tocando!");
+    }
+    public void atenderAparelho() {
+      if (atender=true) {
+        System.out.printIn("Você atendeu essa ligação... Aguente");
+      } else {
+        System.out.printIn("Eita, você rejeitou alguém");
+        System.out.printIn("Essa ligação foi redirecionada para o correio de voz. De nada!");
+      }
+    }
 }
